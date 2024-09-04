870292b446beb67a0c51192f47ed7f5e7975a25a
Marco-Guarnera <Darkaquon93@gmail.com>
2024-09-05 00:17:12 +0200

    Completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,49 @@
 // Fase di preparazione
 
-// Variabili
-// Strutture dati
-// Funzioni
+const app = Vue.createApp({
+    name: "Vue Slider",
+    data: () => ({
+        currentSrc: 0,
+        imgsList: [
+            {
+                img: "../img/img01.jpg",
+                title: "Marvel's Spiderman Miles Morale",
+                txt: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man."
+            },
 
-// Fase di gestione eventi
-// Fase di raccolta dati
-// Fase di elaborazione
-// Fase di produzione
+            {
+                img: "../img/img02.jpg",
+                title: "Ratchet & Clank: Rift Apart",
+                txt: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality."
+            },
+
+            {
+                img: "../img/img03.jpg",
+                title: "Fortnite",
+                txt: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos."
+            },
+
+            {
+                img: "../img/img04.jpg",
+                title: "Stray",
+                txt: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city"
+            },
+
+            {
+                img: "../img/img05.jpg",
+                title: "Marvel's Avengers",
+                txt: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay."
+            }
+        ]
+    }),
+    methods: {
+        prev() {
+            return this.currentSrc === 0 ? this.currentSrc = this.imgsList.length - 1 : this.currentSrc--;
+        },
+        next() {
+            return this.currentSrc === this.imgsList.length - 1 ? this.currentSrc = 0 : this.currentSrc++;
+        }
+    }
+});
+
+app.mount("#slider");
